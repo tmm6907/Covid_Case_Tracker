d22162b5591ca646546615d350e4bc88b0aacf90
tmm6907 <tmm6907@gmail.com>
2021-02-25 14:24:09 -0500

    more dependencies and the beginnings of the webscraping code

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #main.py
-
+import requests
 from bs4 import BeautifulSoup
 
 with open('mdcovid.mhtml', 'r') as html_file:
-    content = html_file.read()
-    soup = BeautifulSoup(content, 'lxml')
-    print(soup.prettify())
+    soup = BeautifulSoup(html_file,'lxml')
+    counties = soup.find_all('div')
+    #for county in counties:
+    #    countyname = county.find('p').text
+    #    print(countyname, '/n')
+    print(counties)
