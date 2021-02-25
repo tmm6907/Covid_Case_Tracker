c619e5b50b6037fa1f96e19f33cc05881929c219
tmm6907 <tmm6907@gmail.com>
2021-02-25 13:51:55 -0500

    Attempting to get bs4 imported

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #main.py
+
+from bs4 import BeautifulSoup
+
+with open('mdcovid.mhtml', 'r') as html_file:
+    content = html_file.read()
+    soup = BeautifulSoup(content, 'lxml')
+    print(soup.prettify())
