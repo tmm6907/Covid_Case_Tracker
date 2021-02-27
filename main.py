c1ecc3107ad778a2cd3657f620c33f9e378e3918
tmm6907 <tmm6907@gmail.com>
2021-02-27 08:07:43 -0500

    Set up logic for Prince George's County

--- main.py
+++ main.py
@@ -2,10 +2,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-with open('mdcovid.mhtml', 'r') as html_file:
-    soup = BeautifulSoup(html_file,'lxml')
-    counties = soup.find_all('div')
-    #for county in counties:
-    #    countyname = county.find('p').text
-    #    print(countyname, '/n')
-    print(counties)
+html_file = requests.get('https://www.wbaltv.com/article/covid-19-numbers-maryland-map-graphs-faq-february-22-28/35586153#').text
+
+#with open('mdcovid.mhtml', 'r') as html_file:
+soup = BeautifulSoup(html_file,'lxml')
+soup.prettify()
+region = soup.find('div', class_='article-content--body-text')
+elements = region.find_all('p')
+countyname= ''
+for element in elements:
+    if 'Prince' in element.text:
+        countyfname = element.text.split()[0]
+        countylname = element.text.split()[1]
+        cases = element.text.split()[2]
+        deaths = element.text.split()[3]
+        probdeaths = element.text.split()[4]
+        
+        print(f"The statistics for {countyfname} {countylname} are:")
+        print(f"Cases: {cases} ")
+        print(f"Deaths: {deaths} ")
+        print(f"Probable Deaths: {probdeaths} ")
+
