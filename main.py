526ef4d452d64db582e6dff13523390be2696d39
tmm6907 <tmm6907@gmail.com>
2021-03-03 13:26:56 -0500

    Comments

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import requests
 import time
 
 def covidpg(index):
-    #keeps track of log number
 
     #Retrieve html file from webpage and initialize soup object on that file
     html_file = requests.get('https://www.wbaltv.com/article/covid-19-numbers-maryland-map-graphs-faq-february-22-28/35586153#').text
@@ -14,7 +13,7 @@ def covidpg(index):
     region = soup.find('div', class_='article-content--body-text')
     start = region.find('p', string='Allegany  6,409       (198)        1*')
     
-    while('Data not available' not in start.text):
+    while('Data not available' not in start.text): #isolates list of counties from rest of document
         if start.text.split()[1].isalpha():
             countyname = (start.text.split()[0]) + ' ' + (start.text.split()[1])
             cases = start.text.split()[2]
@@ -26,7 +25,8 @@ def covidpg(index):
             deaths = start.text.split()[2]
             probdeaths = start.text.split()[3]
         
-        nl = '\n'
+        #logging data into a folder of files that increment every day
+        nl = '\n' #new line character
         logger = open(f'logs/log{index}.txt', 'a')
         logger.write(f'The statistics for {countyname} county are:{nl}')
         logger.write(f'Cases: {cases}{nl}')
@@ -34,6 +34,8 @@ def covidpg(index):
         logger.write(f'Probable Deaths: {probdeaths}{nl}')
         
         start = start.next_sibling
+
+#print timestamp
 localt = time.localtime()
 print(f"File Saved {localt}...")
 
--- runcovidpg.py
+++ runcovidpg.py
@@ -2,10 +2,10 @@ from main import covidpg
 import time
 
 def main():
-    index = 1
+    index = 1 #keeps track of log number
     while True:
         covidpg(index)
         index+=1
         time_wait = 60
-        time.sleep(time_wait*1440)
+        time.sleep(time_wait*1440) #runs once a day
 main()
